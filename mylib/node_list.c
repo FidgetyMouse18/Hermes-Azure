cfc3931615f0d85569aa5ed2f10d36d701eb738d
Joshua Pinti <joshua.pinti@student.uq.edu.au>
2025-05-23 17:39:18 +1000

    bada bing bada boom

---
+++ mylib/node_list.c
@@ -0,0 +1,59 @@
+#include "node_list.h"
+
+sys_dlist_t node_list = SYS_DLIST_STATIC_INIT(&node_list);
+
+int node_add(const uint8_t *uuid, const uint16_t timestamp)
+{
+    struct node_node *current;
+    SYS_DLIST_FOR_EACH_CONTAINER(&node_list, current, node)
+    {
+        if (memcmp(current->uuid, uuid, BLE_UUID_LEN) == 0)
+        {
+            return -EEXIST;
+        }
+    }
+
+    struct node_node *new_node = k_malloc(sizeof(struct node_node));
+    if (!new_node)
+    {
+        return -ENOMEM;
+    }
+
+    memcpy(new_node->uuid, uuid, BLE_UUID_LEN);
+    new_node->timestamp = timestamp;
+
+    sys_dlist_append(&node_list, &new_node->node);
+    return 0;
+}
+
+int node_remove(const uint8_t *uuid)
+{
+    struct node_node *current;
+    SYS_DLIST_FOR_EACH_CONTAINER(&node_list, current, node)
+    {
+        if (memcmp(current->uuid, uuid, BLE_UUID_LEN) == 0)
+        {
+            sys_dlist_remove(&current->node);
+            k_free(current);
+            return 0;
+        }
+    }
+    return -ENOENT;
+}
+
+int check_node(const uint8_t *uuid, const uint16_t timestamp)
+{
+    struct node_node *current;
+    SYS_DLIST_FOR_EACH_CONTAINER(&node_list, current, node)
+    {
+        if (memcmp(current->uuid, uuid, BLE_UUID_LEN) == 0)
+        {
+            if (timestamp == current->timestamp)
+            {
+                return 0;
+            }
+            return -2;
+        }
+    }
+    return -1;
+}
---
+++ mylib/node_list.h
@@ -0,0 +1,27 @@
+#ifndef NODE_LIST_H
+#define NODE_LIST_H
+
+#include <zephyr/sys/dlist.h>
+#include <zephyr/kernel.h>
+#include <stdint.h>
+#include <zephyr/shell/shell.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLE_UUID_LEN 4
+
+struct node_node
+{
+    sys_dnode_t node;
+    uint8_t uuid[BLE_UUID_LEN];
+    uint16_t timestamp;
+};
+
+extern sys_dlist_t node_list;
+int node_add(const char *name, const uint8_t *mac, uint16_t major, uint16_t minor,
+                float x, float y, const char *left, const char *right);
+int node_remove(const uint8_t *mac);
+int check_node(const uint8_t *uuid, const uint16_t timestamp);
+
+#endif
